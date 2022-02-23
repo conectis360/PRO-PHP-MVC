9b7d22b9e42d9638b697094f1f9930fedfe64b53
conectis360 <conectis360@gmail.com>
2022-02-23 20:58:09 -0300

    finishing testing chapter with symfony panther to test browser

--- resources/views/users/register.advanced.php
+++ resources/views/users/register.advanced.php
@@ -5,55 +5,19 @@
         <h1 class="text-3xl font-bold">
             Register
         </h1>
-        <form
-            method="post"
-            action="{{ $registerAction }}"
-            class="flex flex-col w-full space-y-4 max-w-xl"
-        >
-            @if(isset($_SESSION['register_errors']))
-                <ol class="list-disc text-red-500">
-                    @foreach($_SESSION['register_errors'] as $field => $errors)
-                        @foreach($errors as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    @endforeach
-                </ol>
+        <form method="post" action="{{ $logInAction }}" class="flex flex-col w-full space-y-4 max-w-xl log-in-form">
+            @if(isset($_SESSION['login_errors']))
+            <ol class="list-disc text-red-500 log-in-errors">
+                @foreach($_SESSION['login_errors'] as $field => $errors)
+                @foreach($errors as
+                $error)
+                <li>{{ $error }}</li>
+                @endforeach @endforeach
+            </ol>
             @endif
-            <input type="hidden" name="csrf" value="{{ $csrf }}" />
-            <label for="name" class="flex flex-col w-full">
-                <span class="flex">Name:</span>
-                <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    class="bg-gray-50 rounded-lg p-2 text-gray-900"
-                    placeholder="Alex"
-                />
-            </label>
-            <label for="email" class="flex flex-col w-full">
-                <span class="flex">Email:</span>
-                <input
-                    id="email"
-                    name="email"
-                    type="text"
-                    class="bg-gray-50 rounded-lg p-2 text-gray-900"
-                    placeholder="alex.42@gmail.com"
-                />
-            </label>
-            <label for="password" class="flex flex-col w-full">
-                <span class="flex">Password:</span>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="bg-gray-50 rounded-lg p-2 text-gray-900"
-                />
-            </label>
-            <button
-                type="submit"
-                class="bg-indigo-500 rounded-lg p-2 text-white"
-            >
-                Register
+            <!-- ... -->
+            <button type="submit" class="bg-indigo-500 rounded-lg p-2 text-white log-in-button">
+                Log in
             </button>
         </form>
     </div>
@@ -61,44 +25,26 @@
         <h1 class="text-3xl font-bold">
             Log in
         </h1>
-        <form
-            method="post"
-            action="{{ $logInAction }}"
-            class="flex flex-col w-full space-y-4 max-w-xl"
-        >
+        <form method="post" action="{{ $logInAction }}" class="flex flex-col w-full space-y-4 max-w-xl">
             @if(isset($_SESSION['login_errors']))
-                <ol class="list-disc text-red-500">
-                    @foreach($_SESSION['login_errors'] as $field => $errors)
-                        @foreach($errors as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    @endforeach
-                </ol>
+            <ol class="list-disc text-red-500">
+                @foreach($_SESSION['login_errors'] as $field => $errors)
+                @foreach($errors as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+                @endforeach
+            </ol>
             @endif
             <input type="hidden" name="csrf" value="{{ $csrf }}" />
             <label for="email" class="flex flex-col w-full">
                 <span class="flex">Email:</span>
-                <input
-                    id="email"
-                    name="email"
-                    type="text"
-                    class="bg-gray-50 rounded-lg p-2 text-gray-900"
-                    placeholder="alex.42@gmail.com"
-                />
+                <input id="email" name="email" type="text" class="bg-gray-50 rounded-lg p-2 text-gray-900" placeholder="alex.42@gmail.com" />
             </label>
             <label for="password" class="flex flex-col w-full">
                 <span class="flex">Password:</span>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="bg-gray-50 rounded-lg p-2 text-gray-900"
-                />
+                <input id="password" name="password" type="password" class="bg-gray-50 rounded-lg p-2 text-gray-900" />
             </label>
-            <button
-                type="submit"
-                class="bg-indigo-500 rounded-lg p-2 text-white"
-            >
+            <button type="submit" class="bg-indigo-500 rounded-lg p-2 text-white">
                 Log in
             </button>
         </form>
